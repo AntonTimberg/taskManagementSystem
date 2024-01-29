e6f5d5201539ea132e17d42b5ce5d787fac19551
AntonTimberg <antonoffice2015@gmail.com>
2024-01-29 18:35:25 +0300

    -

--- src/main/java/com/example/taskmanagementsystem/task/TaskRepo.java
+++ src/main/java/com/example/taskmanagementsystem/task/TaskRepo.java
@@ -4,7 +4,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 
 public interface TaskRepo extends JpaRepository<Task, Long> {
     Page<Task> findByAuthorEmail(String authorEmail, Pageable pageable);
